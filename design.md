5b9fef4d11e30bfd9850fc2ff8c237e92fe5dc8e
Michael Cupka <michaelcupka@hotmail.com>
2024-04-12 20:38:16 -0400

    Initial commit

---
+++ design.md
@@ -0,0 +1,52 @@
+=============
+Tetris Game
+=============
+
+Test to help me learn bevy / rust. Make a tetris clone.
+
+
+Plan
+----
+☑☐
+☑1. Make a small rectangle appear on the screen
+☐2. Make a rectangle fall from the top of the screen to the bottom
+☐3. Allow user to move it while it is in mid-air
+☐4. Make blocks drop repeatedly
+☐5. Make blocks collide with each other
+☐6. Create new block shapes
+☐7. Allow user to rotate blocks in mid-air
+☐8. Make game end when blocks hit top
+☐9. Create a scoring system
+☐10. Save scores
+
+Make a small rectangle appear on the screen
+-------------------------------------------
+Use a mesh / Mesh2D?
+
+Make a rectangle fall from the top of the screen to the bottom
+--------------------------------------------------------------
+
+
+Allow user to move it while it is in mid-air
+------------------------------------------
+
+Make blocks drop repeatedly
+---------------------------
+
+Make blocks collide with each other
+------------------------------------
+
+Create new block shapes
+-----------------------
+
+Allow user to rotate blocks in mid-air
+--------------------------------------
+
+Make game end when blocks hit top
+----------------------------------
+
+Create a scoring system
+------------------------
+
+ Save scores
+-------------
---
+++ src/main.rs
@@ -0,0 +1,113 @@
+use bevy::{
+    prelude::*,
+    sprite::{MaterialMesh2dBundle, Mesh2dHandle}, window::WindowResolution,
+};
+use rand::prelude::*;
+
+#[derive(Component)]
+struct MainGameCamera;
+
+#[derive(Resource)]
+struct BlockSpawnTimer(Timer);
+#[derive(Resource)]
+struct ColorTimer(Timer);
+
+#[derive(Component)]
+struct PlayerRect;
+
+#[derive(Resource)]
+struct RectangleMesh(Mesh2dHandle);
+
+#[derive(Resource)]
+struct RectangleColorMaterial(Handle<ColorMaterial>);
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    // Spawn camera
+    commands.spawn((Camera2dBundle::default(), MainGameCamera));
+
+    // Create mesh resources
+    let rectangle_mesh_handle = meshes.add(Rectangle {
+        half_size: [50.0, 50.0].into(),
+    });
+
+    // Create color material resource
+    let rectangle_material_handle = materials.add(Color::rgba(0.8, 0.1, 0.1, 1.0));
+
+    // Resource to directly access handle of the rectangle mesh
+    commands.insert_resource(RectangleMesh(Mesh2dHandle(rectangle_mesh_handle.clone())));
+    commands.insert_resource(RectangleColorMaterial(rectangle_material_handle.clone()));
+    commands.insert_resource(ColorTimer(Timer::from_seconds(0.1, TimerMode::Repeating)));
+    commands.insert_resource(BlockSpawnTimer(Timer::from_seconds(
+        2.0,
+        TimerMode::Repeating,
+    )));
+}
+
+fn change_rectangle_color_periodically(
+    time: Res<Time>,
+    mut color_timer: ResMut<ColorTimer>,
+    query: Query<(Entity, &Handle<ColorMaterial>), With<PlayerRect>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut commands: Commands,
+) {
+    color_timer.0.tick(time.delta());
+    if color_timer.0.just_finished() {
+        // Change rect  color
+        for (entity, material_handle) in query.iter() {
+            let current_color = materials.remove(material_handle).unwrap().color;
+            let new_color = current_color + Color::rgba(0.01, 0.01, 0.01, 0.01);
+
+            commands.entity(entity).insert(materials.add(ColorMaterial {
+                color: new_color,
+                ..default()
+            }));
+        }
+    }
+}
+
+// Periodically spawn blocks
+fn spawn_blocks_periodically(
+    mut commands: Commands,
+    block_color: Res<RectangleColorMaterial>,
+    block_mesh: Res<RectangleMesh>,
+    time: Res<Time>,
+    mut block_timer: ResMut<BlockSpawnTimer>,
+) {
+    block_timer.0.tick(time.delta());
+    let x_transform: f32 = rand::thread_rng().gen_range(-100.0..100.0);
+    if block_timer.0.just_finished() {
+        println!("Spawning a block!");
+        println!("{x_transform}");
+        commands.spawn(MaterialMesh2dBundle {
+            mesh: block_mesh.0.clone(),
+            material: block_color.0.clone(),
+            transform: Transform::from_xyz(
+            x_transform,
+            100.0, 0.0),
+            ..default()
+        });
+    }
+}
+
+fn main() {
+    println!("Tetris Clone");
+    App::new()
+        .add_plugins(DefaultPlugins.set(
+            WindowPlugin {
+                primary_window: Some(Window {
+                    resolution: WindowResolution::new(1500.0, 1500.0),
+                    ..default()
+                }),
+                ..default()
+            }
+        )
+        )
+        .add_systems(Startup, setup)
+        .add_systems(Update, change_rectangle_color_periodically)
+        .add_systems(Update, spawn_blocks_periodically)
+        .run();
+}
