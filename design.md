d008241e529756393ca2e1a7443f01578db68ad2
Michael Cupka <michaelcupka@hotmail.com>
2024-04-29 22:43:00 -0400

    Make blocks collide with each other
        - Add ideas for implementation to design.md

--- design.md
+++ design.md
@@ -9,10 +9,10 @@ Plan
 ----
 ☑☐
 ☑1. Make a small rectangle appear on the screen
-☐2. Make a rectangle fall from the top of the screen to the bottom
+☑2. Make a rectangle fall from the top of the screen to the bottom
 ☐3. Allow user to move it while it is in mid-air
-☐4. Make blocks drop repeatedly
-☐5. Make blocks collide with each other
+☑4. Make blocks drop repeatedly
+☑5. Make blocks collide with each other
 ☐6. Create new block shapes
 ☐7. Allow user to rotate blocks in mid-air
 ☐8. Make game end when blocks hit top
@@ -25,16 +25,27 @@ Use a mesh / Mesh2D?
 
 Make a rectangle fall from the top of the screen to the bottom
 --------------------------------------------------------------
-
+move_blocks_down system drops block and checks for collision / reaching the
+bottom. Probably need to move collision detection to new function for more
+complex shapes and then use something like rapier for doing detection and maybe
+dropping? Rapier is a physics engine with a bevy plugin.
 
 Allow user to move it while it is in mid-air
-------------------------------------------
+--------------------------------------------
+How to do user input?
+Need to mark one block as the "current" block? There should only be one block
+    falling at a time and that is the current block.
+Make system that checks for user input and modifies transform of current block
+    to move it left or right.
 
 Make blocks drop repeatedly
 ---------------------------
+spawn_blocks_periodically system uses BlockSpawnTimer to know when to spawn a
+new block
 
 Make blocks collide with each other
 ------------------------------------
+Should switch to rapier for using more complex shapes
 
 Create new block shapes
 -----------------------
@@ -44,9 +55,13 @@ Allow user to rotate blocks in mid-air
 
 Make game end when blocks hit top
 ----------------------------------
+Probably do this in the move_blocks_down system or spawn_blocks_periodically
+Just check if the current block stops above a threshold
 
 Create a scoring system
 ------------------------
+Score == number of blocks placed?
 
  Save scores
 -------------
+When the game ends, prompt for initials and save score in leaderboard
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use bevy::{
     prelude::*,
-    sprite::{MaterialMesh2dBundle, Mesh2dHandle},
+    sprite::{MaterialMesh2dBundle, Mesh2d, Mesh2dHandle},
     window::WindowResolution,
 };
 use rand::prelude::*;
@@ -11,7 +11,15 @@ struct MainGameCamera;
 #[derive(Resource)]
 struct BlockSpawnTimer(Timer);
 #[derive(Resource)]
+struct GravityTimer(Timer);
+#[derive(Resource)]
 struct ColorTimer(Timer);
+
+#[derive(Component)]
+struct Block;
+
+#[derive(Component)]
+struct Falling;
 
 #[derive(Component)]
 struct PlayerRect;
@@ -34,7 +42,6 @@ fn setup(
     let rectangle_mesh_handle = meshes.add(Rectangle {
         half_size: [50.0, 50.0].into(),
     });
-
     // Create color material resource
     let rectangle_material_handle = materials.add(Color::rgba(0.8, 0.1, 0.1, 1.0));
 
@@ -42,8 +49,12 @@ fn setup(
     commands.insert_resource(RectangleMesh(Mesh2dHandle(rectangle_mesh_handle.clone())));
     commands.insert_resource(RectangleColorMaterial(rectangle_material_handle.clone()));
     commands.insert_resource(ColorTimer(Timer::from_seconds(0.1, TimerMode::Repeating)));
-    commands.insert_resource(BlockSpawnTimer(Timer::from_seconds(
+    commands.insert_resource(GravityTimer(Timer::from_seconds(
         0.01,
+        TimerMode::Repeating,
+    )));
+    commands.insert_resource(BlockSpawnTimer(Timer::from_seconds(
+        0.5,
         TimerMode::Repeating,
     )));
 }
@@ -83,12 +94,55 @@ fn spawn_blocks_periodically(
     if block_timer.0.just_finished() {
         println!("Spawning a block!");
         println!("{x_transform}");
-        commands.spawn(MaterialMesh2dBundle {
-            mesh: block_mesh.0.clone(),
-            material: block_color.0.clone(),
-            transform: Transform::from_xyz(x_transform, x_transform, 0.0),
-            ..default()
-        });
+        commands.spawn((
+            MaterialMesh2dBundle {
+                mesh: block_mesh.0.clone(),
+                material: block_color.0.clone(),
+                transform: Transform::from_xyz(x_transform, 300.0, 0.0),
+                ..default()
+            },
+            Block,
+            Falling,
+        ));
+    }
+}
+
+// Gravity for blocks
+fn move_blocks_down(
+    mut commands: Commands,
+    time: Res<Time>,
+    mut gravity_timer: ResMut<GravityTimer>,
+    mut falling_block_query: Query<(Entity, &mut Transform), (With<Block>, With<Falling>)>,
+    stationary_block_query: Query<&Transform, (With<Block>, Without<Falling>)>,
+    //TODO: make a new query without falling. Iterate over this for collision
+    // detection instead of using iter_combinations_mut()
+) {
+    gravity_timer.0.tick(time.delta());
+    if gravity_timer.0.just_finished() {
+        for (entity, mut transform) in falling_block_query.iter_mut() {
+            transform.translation.y -= 1.0;
+            if transform.translation.y <= -300.0 {
+                commands.entity(entity).remove::<Falling>();
+            }
+            for transform_other in stationary_block_query.iter() {
+                let (x1, x2, y1, y2, w1, w2, h1, h2) = (
+                    transform.translation.x,
+                    transform_other.translation.x,
+                    transform.translation.y,
+                    transform_other.translation.y,
+                    100.0,
+                    100.0,
+                    100.0,
+                    100.0,
+                );
+                if (x1 + w1 / 2.0 >= x2 - w2 / 2.0)
+                    && (x1 - w2 / 2.0 <= x2 + w2 / 2.0)
+                    && (y1 - h1 / 2.0 <= y2 + h2 / 2.0)
+                {
+                    commands.entity(entity).remove::<Falling>();
+                }
+            }
+        }
     }
 }
 
@@ -105,5 +159,6 @@ fn main() {
         .add_systems(Startup, setup)
         .add_systems(Update, change_rectangle_color_periodically)
         .add_systems(Update, spawn_blocks_periodically)
+        .add_systems(Update, move_blocks_down)
         .run();
 }
